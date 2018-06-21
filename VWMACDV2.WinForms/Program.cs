52377a5d492018504c0f41a1d15aaa565c4dc55c
ceisakatirci <ceisakatirci@gmail.com>
2018-06-21 09:36:13 +0300

    .

--- VWMACDV2.WinForms/Program.cs
+++ VWMACDV2.WinForms/Program.cs
@@ -14,6 +14,7 @@ namespace VWMACDV2.WinForms
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
