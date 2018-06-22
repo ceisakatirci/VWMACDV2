7f5903976e211411d39b3bc45b95f23ccbfd6a65
ceisakatirci <ceisakatirci@gmail.com>
2018-06-22 16:47:23 +0300

    .

--- VWMACDV2.WinForms/EpostaSorumlusu.cs
+++ VWMACDV2.WinForms/EpostaSorumlusu.cs
@@ -14,31 +14,38 @@ namespace VWMACDV2.WinForms
     {
         public static void Gonder(string mesaj)
         {
-            SmtpClient sc = new SmtpClient();
-            sc.Port = 587;
-            sc.Host = "smtp.gmail.com";
-            sc.EnableSsl = true;
-            sc.UseDefaultCredentials = false;
-            sc.Credentials = new NetworkCredential("adayazilim.isa@gmail.com", "kB*1928374655");
+            //SmtpClient sc = new SmtpClient();
+            //sc.Port = 587;
+            //sc.Host = "smtp.gmail.com";
+            //sc.EnableSsl = true;
+            //sc.UseDefaultCredentials = false;
+            //sc.Credentials = new NetworkCredential("adayazilim.isa@gmail.com", "***");
 
-            MailMessage mail = new MailMessage();
+            //MailMessage mail = new MailMessage();
 
 
-            mail.From = new MailAddress("mustafa@adayazilim.com", "Mustafa Kemal Yıldız");
+            //mail.From = new MailAddress("mustafa@adayazilim.com", "Mustafa Kemal Yıldız");
 
-            mail.ReplyToList.Add("mustafa@adayazilim.com");
+            //mail.ReplyToList.Add("mustafa@adayazilim.com");
 
-            mail.To.Add("adayazilim.isa@gmail.com");
-            //mail.CC.Add("alici3@mail.com");
-            mail.Subject = "E-Posta Konusu";
-            mail.IsBodyHtml = true;
-            mail.Body = "E-Posta İçeriği";
-       
+            //mail.To.Add("adayazilim.isa@gmail.com");
+            ////mail.CC.Add("alici3@mail.com");
+            //mail.Subject = "E-Posta Konusu";
+            //mail.IsBodyHtml = true;
+            //mail.Body = "E-Posta İçeriği";
 
-            mail.Attachments.Add(new Attachment(@"DOA- Acente_Police_XML_Ciktisi_20180608_0928_7041400.xml"));
-            //mail.Attachments.Add(new Attachment(@"C:\Sonuc.pptx"));
 
-            sc.Send(mail);
+            //var attachment = new Attachment(@"DOA- Acente_Police_XML_Ciktisi_20180608_0928_7041400.xml");
+            //string contentID = "test001@host";
+            //attachment.ContentId = contentID;
+            //mail.Attachments.Add(attachment);
+            ////mail.Attachments.Add(new Attachment(@"C:\Sonuc.pptx"));
+            //mail.IsBodyHtml=true;
+
+
+
+
+            //sc.Send(mail);
         }
     }
 
